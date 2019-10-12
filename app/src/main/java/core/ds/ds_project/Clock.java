527c9c3e0f1a5b38d5a45d0dfac4d4c3151347b4
Sebastian Siwik <sebastian.siwik11@gmail.com>
2019-10-12 17:03:59 +0200

    Make running the clock schedule ticks every two seconds

--- app/src/main/java/core/ds/ds_project/Clock.java
+++ app/src/main/java/core/ds/ds_project/Clock.java
@@ -1,4 +1,42 @@
 package core.ds.ds_project;
 
-public class Clock {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public final class Clock {
+    private Date date;
+    private PropertyChangeSupport support;
+    private ScheduledExecutorService executor;
+
+    Clock() {
+        date = new Date();
+        support = new PropertyChangeSupport(this);
+        executor = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public void addPropertyChangeListener(final PropertyChangeListener pcl) {
+        support.addPropertyChangeListener(pcl);
+    }
+    public void removePropertyChangeListener(final PropertyChangeListener pcl) {
+        support.removePropertyChangeListener(pcl);
+    }
+
+    private void tick() {
+        date = new Date();
+        System.out.println(date);
+    }
+
+    void run() {
+        executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                tick();
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+
+    }
 }
