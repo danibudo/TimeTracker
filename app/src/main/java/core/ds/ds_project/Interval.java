cdcfed6cfd8eac340ed24b898fe0d083a021a641
Sebastian Siwik <sebastian.siwik11@gmail.com>
2019-10-12 21:18:20 +0200

    Create Interval prototype logic

--- app/src/main/java/core/ds/ds_project/Interval.java
+++ app/src/main/java/core/ds/ds_project/Interval.java
@@ -1,12 +1,37 @@
 package core.ds.ds_project;
 
-import java.util.Date;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class Interval {
+public class Interval implements PropertyChangeListener {
+    private long startTime;
+    private long endTime;
 
-    private Date finalDate;
-    private Date beginDate;
+    public Interval(final long start, final long finish) {
+        startTime = start;
+        endTime = finish;
+    }
 
+    public long getDuration() {
+        //Difference in milliseconds
+        return endTime - startTime;
+    }
+
+    public long getRemainingTime() {
+        long currentTime = Clock.getCurrentTime();
+
+        if (currentTime < startTime) return getDuration();
+        else if (currentTime > endTime) return 0;
+        else return endTime - currentTime;
+    }
+
+    @Override
+    public void propertyChange(final PropertyChangeEvent propertyChangeEvent) {
+        //Temporary solution for testing purposes
+        if (getRemainingTime() > 0 && getRemainingTime() <= getDuration()) {
+            System.out.println(getRemainingTime() / 1000 + "seconds remaining");
+        }
+    }
 
     //getFinalDate() {}
     //getBeginDate() {}
