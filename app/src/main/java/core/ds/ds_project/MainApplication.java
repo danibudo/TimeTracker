e20eb143630e95e18546b6e5495bd890c66e3342
Sebastian Siwik <sebastian.siwik11@gmail.com>
2019-10-29 12:01:42 +0100

    Prepare main for test a1

--- app/src/main/java/core/ds/ds_project/MainApplication.java
+++ app/src/main/java/core/ds/ds_project/MainApplication.java
@@ -21,14 +21,11 @@ public class MainApplication {
         Activity task1 = new Task((Project) project2, "T1");
         ((Project) project2).addActivity(task2);
         ((Project) project2).addActivity(task1);
-
-        Activity task4 = new Task((Project) project1, "T4");
-        ((Project) project1).addActivity(task4);
+        ((Project) project1).addActivity(project2);
 
         clock.run();
 
         ((Task) task3).start();
-        ((Task) task4).start();
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -36,19 +33,23 @@ public class MainApplication {
         }
         ((Task) task3).stop();
         try {
-            Thread.sleep(3000);
+            Thread.sleep(7000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        ((Task) task4).stop();
-
-        Activity project3 = new Project((Project) project1, "P3");
-        ((Project) project1).addActivity(project3);
-        Activity task5 = new Task((Project) project3, "T5");
-        Activity task6 = new Task((Project) project3, "T6");
-        ((Project) project3).addActivity(task5);
-        ((Project) project3).addActivity(task6);
-        ((Task) task5).start();
-        ((Task) task6).start();
+        ((Task) task2).start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ((Task) task2).stop();
+        ((Task) task3).start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ((Task) task3).stop();
     }
 }
