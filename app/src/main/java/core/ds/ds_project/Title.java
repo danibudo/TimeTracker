345d56cfe2e96b5e4b269c5eedc65eb7dcda6e2f
Sebastian Siwik <sebastian.siwik11@gmail.com>
2019-11-20 18:01:22 +0100

    Add the title field to the Title class

--- app/src/main/java/core/ds/ds_project/Title.java
+++ app/src/main/java/core/ds/ds_project/Title.java
@@ -1,6 +1,13 @@
 package core.ds.ds_project;
 
 public class Title implements Elements {
+    private String title;
+    Title(final String titleName) {
+        title = titleName;
+    }
+    public String getTitle() {
+        return title;
+    }
     @Override
     public void accept(final Text text) {
         /// add implementation
