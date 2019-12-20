f523ea6dd9921712597670407eac15700c2f38c1
Sebastian Siwik <sebastian.siwik11@gmail.com>
2019-12-20 13:01:20 +0100

    Add main dropdown menu

--- app/src/main/java/core/ds/ds_project/MainActivity.java
+++ app/src/main/java/core/ds/ds_project/MainActivity.java
@@ -3,12 +3,19 @@ package core.ds.ds_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
